e5d1ecb8331073949f41ff47306cb5a2dba65954
Siddhi Shah <siddhishah222@gmail.com>
2024-02-23 14:47:37 -0500

    Updates

---
+++ precache-manifest.4b8649037e5c288fad7da2c498926307.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1817a202398c811ceb995eb79995c78b",
+    "url": "/Flight-Search-app/index.html"
+  },
+  {
+    "revision": "0a847d89455ca9005918",
+    "url": "/Flight-Search-app/static/css/2.a8b5d24c.chunk.css"
+  },
+  {
+    "revision": "61122fc1e22bd740efe7",
+    "url": "/Flight-Search-app/static/css/main.7e3ee888.chunk.css"
+  },
+  {
+    "revision": "0a847d89455ca9005918",
+    "url": "/Flight-Search-app/static/js/2.0fa0d43c.chunk.js"
+  },
+  {
+    "revision": "61122fc1e22bd740efe7",
+    "url": "/Flight-Search-app/static/js/main.246ece87.chunk.js"
+  },
+  {
+    "revision": "5e6ab5dfdbf052d42636",
+    "url": "/Flight-Search-app/static/js/runtime~main.d9a2736c.js"
+  },
+  {
+    "revision": "76c388a792ac27e7da933e2704fab7fb",
+    "url": "/Flight-Search-app/static/media/flight.76c388a7.jpg"
+  },
+  {
+    "revision": "9f0294234cf99a41a2ef4b7c50b11279",
+    "url": "/Flight-Search-app/static/media/multiflight.9f029423.png"
+  },
+  {
+    "revision": "a8b2e228713c48f9f76936176e857551",
+    "url": "/Flight-Search-app/static/media/nonstop.a8b2e228.png"
+  }
+]);
