56b0aaac5fa4a742c3d1f07d997eb08f06018fe0
Siddhi Shah <siddhishah222@gmail.com>
2024-02-23 13:54:33 -0500

    Build

---
+++ build/precache-manifest.9a1fa0c9131f674e2f671331fd236b98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4acca28b205c674ea696cbe9de709a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a847d89455ca9005918",
+    "url": "/static/css/2.a8b5d24c.chunk.css"
+  },
+  {
+    "revision": "9e3faac1f8fd2bee011f",
+    "url": "/static/css/main.7e3ee888.chunk.css"
+  },
+  {
+    "revision": "0a847d89455ca9005918",
+    "url": "/static/js/2.0fa0d43c.chunk.js"
+  },
+  {
+    "revision": "9e3faac1f8fd2bee011f",
+    "url": "/static/js/main.a6191371.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76c388a792ac27e7da933e2704fab7fb",
+    "url": "/static/media/flight.76c388a7.jpg"
+  },
+  {
+    "revision": "9f0294234cf99a41a2ef4b7c50b11279",
+    "url": "/static/media/multiflight.9f029423.png"
+  },
+  {
+    "revision": "a8b2e228713c48f9f76936176e857551",
+    "url": "/static/media/nonstop.a8b2e228.png"
+  }
+]);
